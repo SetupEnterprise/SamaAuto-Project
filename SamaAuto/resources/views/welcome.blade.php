b434293f03327b3192e62e870b83e57296a665c8
Rjal95 <fallmodou950@gmail.com>
2020-03-25 10:53:41 +0000

    design

--- SamaAuto/resources/views/welcome.blade.php
+++ SamaAuto/resources/views/welcome.blade.php
@@ -1,82 +1,82 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            h3{
-                color: green;
-            }
-        </style>
+      <meta charset="utf-8">
+      <title>SamaAuto</title>
+      <base href="/">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="<?php echo asset('css/app.css')?>" rel="stylesheet">
+      <link href="<?php echo asset('css/header.component.css')?>" rel="stylesheet">
+      <link href="<?php echo asset('css/home.component.css')?>" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            
+            <nav>
+              <span class="gauchelog">
+                      <p>SAMA AUTO</p>
+              </span>
+              <span class="droitelog">
+                      <a href="#">Se connecter</a>
+              </span>
+                    <!--<a href="#" id="icon"><img src="../../image/plogo.png" width="50rem" height="50rem"/></a>-->
+                      <ul class="ulnave">
+                        <li><a href="#">Points de vente</a></li>
+                        <li><a href="#">Acheter billet</a></li>
+                        <li><a href="#">Les lignes</a></li>
+                      </ul>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Bienvenue sur l'Application SamaAuto!
-                </div>
-
-                <h3>Bon Début...</h3>
+            </nav>
+            <div id="bande">
+                  <div id="logo">
+                    <img id="image" src="<?php echo asset('img/samaautosloganjb1logo.png')?>" width="10%" />
+                  </div>
+                  <div id="recherche">
+                    <form >
+                         <table id='tabsaisi' >
+                            <tr bordercolor='silver'>
+                                <td>
+                                  <input type='text' name='login' required />
+                                </td>
+                                <td>
+                                  <input type='text' name='login' required />
+                                </td>
+                                <td>
+                                     <button type="submit" class="btn btn-success">rechercher</button>
+                                </td>
+                              </tr>
+                           </table>
+                     </form>
+                  </div>
             </div>
+
+        <div id="corps">
+            <div id="menuinfo">
+                <div id="info">
+                  <p>Horaire Mbacke</p>
+                  <p>heure de depart: 20h30</p>
+                  <p>numero: 77 852 23 25</p>
+                </div>
+                <div id="info">
+                  <p>Horaire Tamba</p>
+                  <p>heure de depart: 20h30</p>
+                  <p>numero: 77 852 23 25</p>
+                </div>
+                <div id="info">
+                  <p>Horaire Matam</p>
+                  <p>heure de depart: 20h30</p>
+                  <p>numero: 77 852 23 25</p>
+                </div>
+              </div>
+              <div id="espacemenu">
+                <div id="info">
+                  <p>Horaire Matam</p>
+                  <p>heure de depart: 20h30</p>
+                  <p>numero: 77 852 23 25</p>
+                </div>
+                <div id="image">
+                  <img id="image" src="<?php echo asset('img/samaautosloganjb12.png')?>" width="60%" />
+                </div>
+              </div>
+
         </div>
     </body>
 </html>
