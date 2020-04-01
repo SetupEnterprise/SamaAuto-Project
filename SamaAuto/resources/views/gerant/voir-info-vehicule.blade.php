18c616bad2e551de63b12c2bbc3b6718674492ed
Ouzy012 <ousmane.ndiaye8518@gmail.com>
2020-04-01 15:22:51 +0000

    frontend infos personnelles user

--- SamaAuto/resources/views/gerant/voir-info-vehicule.blade.php
+++ SamaAuto/resources/views/gerant/voir-info-vehicule.blade.php
@@ -70,17 +70,3 @@
 
 @endsection
 
-{{-- <h3></h3>
-<h3></h3>
-<h3></h3>
-<h3>Image véhicule
-
- </h3>
- <br>
-<a class="btn btn-primary"
-    href="{{ route('vehicule.edit',['vehicule'=>$voirVehicule->vehicules_id.' '.$voirVehicule->categories_id]) }}" role="button">
-    Modifier
-</a><br>
-<a class="btn btn-danger" href="#" role="button">
-    Supprimer
-</a> --}}
--- SamaAuto/routes/web.php
+++ SamaAuto/routes/web.php
@@ -26,11 +26,13 @@ Route::get('/sign_in', function () {
 Route::resource('gerant','GerantsController');
 Route::resource('categorie','CategorieController');
 Route::resource('vehicule','VehiculesController');
+Route::resource('trajet','TrajetsController');
+Route::resource('arret','ArretsController');
+Route::resource('voyage','VoyagesController');
+Route::resource('user','UsersController');
+
 
 
 Route::get('/sign_up', 'UsersController@create')->name('sign_up');
 Route::post('/sign_up', 'UsersController@store')->name('sign_up');
 
-Route::resource('trajet','TrajetsController');
-Route::resource('arret','ArretsController');
-Route::resource('voyage','VoyagesController');
